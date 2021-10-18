a08ddc3b55e309c71bd5f0c11807902acd1c2324
unknown <rajendra.boke@net-mon.net>
2021-10-18 19:03:56 +0530

    done file path change

--- RDBMS-Log-Shipper/requirement.sh
+++ RDBMS-Log-Shipper/requirement.sh
@@ -2,21 +2,22 @@
 
 echo "Installing virtualenv"
 
-setup_path = "$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")"
-
-wheel_path="$setup_path/requirements/pip/"
+wheel_path="$PWD/requirements/pip/"
 pip3 install --no-index --find-links=file:"$wheel_path" virtualenv
 
 echo "creating python3 instance rdbms_connector "
-cd $setup_path/
+cd $PWD
 virtualenv -p python3 .
 
 
 #activate envirnoment
 echo "activate virtualenv rdbms_connector"
-source $setup_path/bin/activate
+source $PWD/bin/activate
 
-apt_path="setup_path/requirements/apt"
+echo "Installing required pip3 packages from requirements.txt "
+pip3 install --no-index --find-links  "$wheel_path" -r $PWD/requirement.txt
+
+apt_path="$PWD/requirements/apt"
 if [[ ! -e /etc/unixODBC ]]; then
   cd $apt_path
   tar -zxvf unixODBC-2.3.4.tar.gz
@@ -25,6 +26,3 @@ if [[ ! -e /etc/unixODBC ]]; then
   make
   make install
 fi
-
-echo "Installing required pip3 packages from requirements.txt "
-pip3 install --no-index --find-links  "$wheel_path" -r $setup_path/requirement.txt
