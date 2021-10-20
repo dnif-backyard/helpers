cecb20e2f017ab0e37a88d0ed6cf0a080764613a
unknown <rajendra.boke@net-mon.net>
2021-10-20 13:53:16 +0530

    pwd in var, unixodbc installed before for pyodbc

--- RDBMS-Log-Shipper/requirement.sh
+++ RDBMS-Log-Shipper/requirement.sh
@@ -2,22 +2,21 @@
 
 echo "Installing virtualenv"
 
-wheel_path="$PWD/requirements/pip/"
+setup_path=$PWD
+
+wheel_path="$setup_path/requirements/pip/"
 pip3 install --no-index --find-links=file:"$wheel_path" virtualenv
 
 echo "creating python3 instance rdbms_connector "
-cd $PWD
+cd $setup_path
 virtualenv -p python3 .
 
 
 #activate envirnoment
 echo "activate virtualenv rdbms_connector"
-source $PWD/bin/activate
+source $setup_path/bin/activate
 
-echo "Installing required pip3 packages from requirements.txt "
-pip3 install --no-index --find-links  "$wheel_path" -r $PWD/requirement.txt
-
-apt_path="$PWD/requirements/apt"
+apt_path="$setup_path/requirements/apt"
 if [[ ! -e /etc/unixODBC ]]; then
   cd $apt_path
   tar -zxvf unixODBC-2.3.4.tar.gz
@@ -26,3 +25,6 @@ if [[ ! -e /etc/unixODBC ]]; then
   make
   make install
 fi
+
+echo "Installing required pip3 packages from requirement.txt"
+pip3 install --no-index --find-links  "$wheel_path" -r $setup_path/requirement.txt
